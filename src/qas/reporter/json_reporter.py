b348e54f478af1251090aeb83ac9a608705222b3
hatlonely <hatlonely@gmail.com>
2022-01-14 22:59:01 +0800

    update

--- src/qas/reporter/json_reporter.py
+++ src/qas/reporter/json_reporter.py
@@ -4,44 +4,46 @@ import json
 from src.qas.assertion.expect import TestResult, CaseResult, StepResult, ExpectResult
 
 
-def report(res: TestResult) -> str:
-    res.summary()
-    return json.dumps(test_summary(res))
+class JsonReporter:
+    @staticmethod
+    def report(res: TestResult) -> str:
+        res.summary()
+        return json.dumps(JsonReporter.test_summary(res))
 
+    @staticmethod
+    def test_summary(res: TestResult) -> dict:
+        return {
+            "isPass": res.is_pass,
+            "succ": res.succ,
+            "fail": res.fail,
+            "name": res.name,
+            "caseResults": [JsonReporter.case_summary(i) for i in res.case_results]
+        }
 
-def test_summary(res: TestResult) -> dict:
-    return {
-        "isPass": res.is_pass,
-        "succ": res.succ,
-        "fail": res.fail,
-        "name": res.name,
-        "caseResults": [case_summary(i) for i in res.case_results]
-    }
+    @staticmethod
+    def case_summary(res: CaseResult) -> dict:
+        return {
+            "isPass": res.is_pass,
+            "case": res.case,
+            "stepResults": [JsonReporter.step_summary(i) for i in res.step_results]
+        }
 
+    @staticmethod
+    def step_summary(res: StepResult) -> dict:
+        return {
+            "isPass": res.is_pass,
+            "step": res.step,
+            "succ": res.succ,
+            "fail": res.fail,
+            "expectResults": [JsonReporter.expect_summary(i) for i in res.expect_results]
+        }
 
-def case_summary(res: CaseResult) -> dict:
-    return {
-        "isPass": res.is_pass,
-        "case": res.case,
-        "stepResults": [step_summary(i) for i in res.step_results]
-    }
-
-
-def step_summary(res: StepResult) -> dict:
-    return {
-        "isPass": res.is_pass,
-        "step": res.step,
-        "succ": res.succ,
-        "fail": res.fail,
-        "expectResults": [expect_summary(i) for i in res.expect_results]
-    }
-
-
-def expect_summary(res: ExpectResult) -> dict:
-    return {
-        "isPass": res.is_pass,
-        "message": res.message,
-        "node": res.node,
-        "val": res.val,
-        "expect": res.expect,
-    }
+    @staticmethod
+    def expect_summary(res: ExpectResult) -> dict:
+        return {
+            "isPass": res.is_pass,
+            "message": res.message,
+            "node": res.node,
+            "val": res.val,
+            "expect": res.expect,
+        }
