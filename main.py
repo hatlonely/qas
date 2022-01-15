401f53ade91bd8be3f9f5acf3b17d08fd1f63214
hatlonely <hatlonely@gmail.com>
2022-01-15 10:53:32 +0800

    update

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 
+import argparse
 from qas.framework import Framework
 
 
 def main():
-    fw = Framework("tmp/env/ali/sample")
+    parser = argparse.ArgumentParser(formatter_class=lambda prog: argparse.RawTextHelpFormatter(prog, width=200), description="""example:
+    qas --test "tmp/env/ali/sample"
+""")
+    parser.add_argument("-t", "--test", help="test directory")
+    parser.add_argument("--context", default="ctx.yaml", help="context file name")
+    parser.add_argument("-c", "--case", help="case name")
+    args = parser.parse_args()
+
+    fw = Framework(args.test)
     fw.run()
 
 
