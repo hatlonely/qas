aa9a2d402b12d86588a98694ea4bdbfe45124ebc
hatlonely <hatlonely@gmail.com>
2022-01-15 19:15:44 +0800

    update

--- qas/driver/mysql_driver.py
+++ qas/driver/mysql_driver.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python3
 
 
+import pymysql
+
 from .default import merge, REQUIRED
 
 
 class MysqlDriver:
+    client: pymysql.Connection
+
     def __init__(self, args):
-        pass
+        args = merge(args, {
+            "host": "localhost",
+            "port": 3306,
+            "username": "root",
+            "password": REQUIRED,
+            "database": REQUIRED,
+        })
+
+        self.client = pymysql.connect(
+            host=args["host"],
+            port=args["port"],
+            user=args["username"],
+            password=args["password"],
+            database=args["database"],
+            cursorclass=pymysql.cursors.DictCursor,
+        )
 
     def do(self, req):
         pass
