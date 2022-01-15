d6c07e8051e326de4125e928ea7d9245321a0862
hatlonely <hatlonely@gmail.com>
2022-01-15 21:24:55 +0800

    update

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md") as fp:
 
 
 with open("LICENSE") as fp:
-    license = fp.read()
+    license_ = fp.read()
 
 
 setup(
@@ -20,6 +20,6 @@ setup(
     author="hatlonely",
     author_email="hatlonely@foxmail.com",
     url="https://github.com/hatlonely/qas",
-    license=license,
+    license=license_,
     packages=find_packages(exclude=("tests", "docs", "tmp"))
 )
