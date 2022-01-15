0da023000b1addb987c0c7d36c822f799afcf264
hatlonely <hatlonely@gmail.com>
2022-01-15 21:32:59 +0800

    update

--- setup.py
+++ setup.py
@@ -21,5 +21,5 @@ setup(
     author_email="hatlonely@foxmail.com",
     url="https://github.com/hatlonely/qas",
     license=license_,
-    packages=find_packages(exclude=("tests", "docs", "tmp"))
+    packages=find_packages(exclude=("tests", "docs", "tmp", "ops"))
 )
