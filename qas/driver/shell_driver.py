41f82a55fc5ac0226c144476ceed741e51e1c5b5
hatlonely <hatlonely@gmail.com>
2022-01-15 17:48:48 +0800

    update

--- qas/driver/shell_driver.py
+++ qas/driver/shell_driver.py
@@ -2,12 +2,29 @@
 
 
 from .default import merge, REQUIRED
+import subprocess
 
 
 class ShellDriver:
+    shebang: str
+    args: list[str]
+
     def __init__(self, args):
-        pass
+        args = merge(args, {
+            "shebang": "/bin/bash",
+            "args": ["-c"],
+        })
+        self.shebang = args["shebang"]
+        self.args = args["args"]
 
     def do(self, req):
-        pass
+        req = merge(req, {
+            "command": REQUIRED,
+        })
+        process = subprocess.run([self.shebang, *self.args, req["command"]], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return {
+            "exit_code": process.returncode,
+            "stdout": process.stdout.decode("utf-8"),
+            "stderr": process.stderr.decode("utf-8"),
+        }
 
---
+++ qas/driver/shell_driver_test.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+
+import json
+import unittest
+from .shell_driver import *
+
+
+class TestSubprocess(unittest.TestCase):
+    def test_bash(self):
+        process = subprocess.run(["/bin/bash", "-c", "echo ${PATH}"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print(process.returncode, process.stdout, process.stderr)
+
+    def test_python(self):
+        process = subprocess.run(["python3", "-c", "print('hello world')"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print(process.returncode, process.stdout, process.stderr)
+
+
+class TestShellDriver(unittest.TestCase):
+    def setUp(self) -> None:
+        self.driver = ShellDriver(args={})
+
+    def test_bash(self):
+        res = self.driver.do(req={
+            "command": "echo hello world"
+        })
+        print(json.dumps(res))
+        self.assertDictEqual(res, {
+            "exit_code": 0,
+            "stdout": "hello world\n",
+            "stderr": "",
+        })
+
+
+class TestShellDriverPython(unittest.TestCase):
+    def setUp(self) -> None:
+        self.driver = ShellDriver(args={
+            "shebang": "python3",
+            "args": ["-c"],
+        })
+
+    def test_python(self):
+        res = self.driver.do(req={
+            "command": """
+a = "hello"
+b = "world"
+print("{} {}".format(a, b))
+"""
+        })
+        print(json.dumps(res))
+        self.assertDictEqual(res, {
+            "exit_code": 0,
+            "stdout": "hello world\n",
+            "stderr": "",
+        })
+
+
+if __name__ == '__main__':
+    unittest.main()
