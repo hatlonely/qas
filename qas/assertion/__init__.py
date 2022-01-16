51ca03924f10c7840cdfe475cc22459bcd0b5bb6
hatlonely <hatlonely@gmail.com>
2022-01-16 22:04:14 +0800

    update

--- qas/assertion/__init__.py
+++ qas/assertion/__init__.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
 from .expect import expect_obj
-from .expect import ExpectResult, StepResult, CaseResult, TestResult
 
-__all__ = ["expect_obj", "ExpectResult", "StepResult", "CaseResult", "TestResult"]
+__all__ = ["expect_obj"]
