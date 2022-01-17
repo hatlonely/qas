769623ca2d9aa41bd0b75794cc0908bb79278fe8
hatlonely <hatlonely@gmail.com>
2022-01-17 17:01:45 +0800

    update

--- qas/driver/shell_driver.py
+++ qas/driver/shell_driver.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
 
-from .default import merge, REQUIRED
+import json
+import yaml
 import subprocess
+
+from .default import merge, REQUIRED
 
 
 class ShellDriver:
@@ -14,7 +17,7 @@ class ShellDriver:
         args = merge(args, {
             "shebang": "/bin/bash",
             "args": ["-c"],
-            "envs": dict[str, str]()
+            "envs": dict[str, str](),
         })
         self.shebang = args["shebang"]
         self.args = args["args"]
@@ -23,17 +26,27 @@ class ShellDriver:
     def do(self, req):
         req = merge(req, {
             "command": REQUIRED,
-            "envs": dict[str, str]()
+            "envs": dict[str, str](),
+            "decoder": "text",
         })
+
         process = subprocess.run(
             [self.shebang, *self.args, req["command"]],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             env=self.envs | req["envs"],
         )
-        return {
+
+        res = {
             "exitCode": process.returncode,
             "stdout": process.stdout.decode("utf-8"),
             "stderr": process.stderr.decode("utf-8"),
         }
+
+        if req["decoder"] == "json":
+            res["json"] = json.loads(res["stdout"])
+        if req["decoder"] == "yaml":
+            res["json"] = yaml.safe_load(res["stdout"])
+
+        return res
 
