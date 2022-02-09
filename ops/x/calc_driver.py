a6da71cc30bb466c3785ef6524551e4db9c5fa29
hatlonely <hatlonely@gmail.com>
2022-02-10 00:21:44 +0800

    update

--- ops/x/calc_driver.py
+++ ops/x/calc_driver.py
@@ -2,5 +2,8 @@ class CalcDriver:
     def __init__(self, args):
         pass
 
+    def default_step_name(self, req):
+        return "echo"
+
     def do(self, req):
         return {"val": req["num1"] + req["num2"]}
--- ops/x/echo_driver.py
+++ ops/x/echo_driver.py
@@ -2,5 +2,8 @@ class EchoDriver:
     def __init__(self, args):
         pass
 
+    def default_step_name(self, req):
+        return "echo"
+
     def do(self, req):
         return req
