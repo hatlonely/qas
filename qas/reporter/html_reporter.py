59ffb2a1568b11d96b9d4581f7616abc7190996b
hatlonely <hatlonely@gmail.com>
2022-02-09 21:22:03 +0800

    update

--- qas/reporter/html_reporter.py
+++ qas/reporter/html_reporter.py
@@ -236,7 +236,7 @@ _case_tpl = """
 _step_tpl = """
 {% if step.is_pass %}
 <a class="card-title btn d-flex justify-content-between align-items-center" data-bs-toggle="collapse" href="#{{ name }}" role="button" aria-expanded="false" aria-controls="{{ name }}">
-    {{ step.name }}
+    {{ step.ctx }} {{ step.name }}
     <span>{% print(durationpy.to_str(step.elapse)) %}</span>
 </a>
 {% else %}
