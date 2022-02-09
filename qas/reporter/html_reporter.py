479fa9afe4a7832774903b77967f8c241329fb33
hatlonely <hatlonely@gmail.com>
2022-02-10 01:13:29 +0800

    update

--- qas/reporter/html_reporter.py
+++ qas/reporter/html_reporter.py
@@ -16,6 +16,17 @@ _report_tpl = """<!DOCTYPE html>
     <title>{{ res.name }} 测试报告</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;700&family=Ubuntu+Mono:wght@400;700&display=swap" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Oswald', sans-serif !important;
+        }
+        pre {
+            font-family: 'Ubuntu Mono', monospace !important;
+        }
+    </style>
 </head>
 
 <body>
