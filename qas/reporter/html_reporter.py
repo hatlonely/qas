8eb165c7778d7718dd61e4f6bafd76af9a77e6ac
hatlonely <hatlonely@gmail.com>
2022-02-09 14:04:24 +0800

    update

--- qas/reporter/html_reporter.py
+++ qas/reporter/html_reporter.py
@@ -4,7 +4,23 @@
 from ..result import TestResult
 from .reporter import Reporter
 
+from jinja2 import Environment, BaseLoader
+
+
+reporter_tpl = """<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>{{ res.name }} 测试报告</title>
+</head>
+
+<body>
+    hello world
+</body>
+</html>
+"""
+
 
 class HtmlReporter(Reporter):
     def report_final_result(self, res: TestResult):
-        pass
+        template = Environment(loader=BaseLoader).from_string(reporter_tpl)
+        print(template.render(res=res))
