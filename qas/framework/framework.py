5f7d175439540abdf5104c04e8e3e685bb9e2e7d
hatlonely <hatlonely@gmail.com>
2022-02-12 01:11:02 +0800

    update

--- qas/framework/framework.py
+++ qas/framework/framework.py
@@ -443,7 +443,6 @@ class Framework:
         step = StepResult(step_info["name"], step_info["ctx"], step_info["description"])
         now = datetime.now()
 
-        # if not parallel:
         if not parallel:
             for req, res in zip(generate_req(step_info["req"]), generate_res(step_info["res"], calculate_num(step_info["req"]))):
                 result = Framework.run_sub_step(req, res, step_info, case, dft, var, ctx, x)
