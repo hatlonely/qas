f44cc6d974857bc319eb4e6a07848a9614ae6055
hatlonely <hatlonely@gmail.com>
2022-02-12 17:35:19 +0800

    update

--- qas/assertion/expect.py
+++ qas/assertion/expect.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 
-from ..result import *
+from ..result import ExpectResult
 from ..util.include import *
 
 
--- qas/assertion/expect_test.py
+++ qas/assertion/expect_test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 from .expect import *
 import unittest
 
