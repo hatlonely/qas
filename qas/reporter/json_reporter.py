063c0c19b03cb0fb308de5d77ab8b3e9243d8941
hatlonely <hatlonely@gmail.com>
2022-02-14 00:42:39 +0800

    update

--- qas/reporter/json_reporter.py
+++ qas/reporter/json_reporter.py
@@ -4,12 +4,16 @@
 import json
 
 from ..result import TestResult
+from ..util import merge
 from .reporter import Reporter
 
 
 class JsonReporter(Reporter):
     def __init__(self, args=None):
-        pass
+        args = merge(args, {
+            "indent": None
+        })
+        self.indent = args["indent"]
 
     def report(self, res: TestResult) -> str:
-        return json.dumps(res, default=lambda x: x.to_json())
+        return json.dumps(res, indent=self.indent, default=lambda x: x.to_json())
