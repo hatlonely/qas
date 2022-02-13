d8af4ee9fa33a01c046b7a1080fa075cf1b956b0
hatlonely <hatlonely@gmail.com>
2022-02-14 00:39:43 +0800

    update

--- qas/reporter/html_reporter.py
+++ qas/reporter/html_reporter.py
@@ -22,16 +22,13 @@ _report_tpl = """<!DOCTYPE html>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;700&family=Ubuntu+Mono:wght@400;700&display=swap" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
+    {{ cfg.font.style }}
     <style>
         body {
-            font-family: 'Oswald', sans-serif !important;
+            font-family: {{ cfg.font.body }};
         }
         pre, code {
-            font-family: 'Ubuntu Mono', monospace !important;
+            font-family: {{ cfg.font.code }};
         }
     </style>
     <script>
@@ -120,9 +117,12 @@ _report_tpl = """<!DOCTYPE html>
             }
         }
     </script>
+
+    {{ cfg.extra.head }}
 </head>
 
 <body>
+    {{ cfg.extra.bodyHeader }}
     <div class="container">
         <div class="row justify-content-md-center">
             <div class="col-lg-10 col-md-12">
@@ -130,6 +130,7 @@ _report_tpl = """<!DOCTYPE html>
             </div>
         </div>
     </div>
+    {{ cfg.extra.bodyFooter }}
 </body>
 
 <script>
@@ -650,9 +651,22 @@ class HtmlReporter(Reporter):
             "stepSeparator": "#",
             "lang": "zh",
             "i18n": {},
+            "font": {
+                "style": """<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;700&family=Ubuntu+Mono:wght@400;700&display=swap" rel="stylesheet">'""",
+                "body": "'Oswald', sans-serif !important",
+                "code": "'Ubuntu Mono', monospace !important",
+            },
+            "extra": {
+                "head": "",
+                "bodyHeader": "",
+                "bodyFooter": "",
+            }
         })
         self.step_separator = args["stepSeparator"]
-        self.i18n = json.loads(json.dumps(merge(merge(i18n["dft"], args["lang"]), args["i18n"])), object_hook=lambda x: SimpleNamespace(**x))
+        self.i18n = json.loads(json.dumps(merge(args["i18n"], merge(i18n["dft"], args["lang"]))), object_hook=lambda x: SimpleNamespace(**x))
+        self.cfg = json.loads(json.dumps(args), object_hook=lambda x: SimpleNamespace(**x))
 
         env = Environment(loader=BaseLoader)
         env.globals.update(format_timedelta=HtmlReporter.format_timedelta)
@@ -667,6 +681,7 @@ class HtmlReporter(Reporter):
         env.globals.update(brief_mode=True)
         env.globals.update(markdown=markdown.markdown)
         env.globals.update(i18n=self.i18n)
+        env.globals.update(cfg=self.cfg)
         self.report_tpl = env.from_string(_report_tpl)
         self.test_tpl = env.from_string(_test_tpl)
         self.case_tpl = env.from_string(_case_tpl)
