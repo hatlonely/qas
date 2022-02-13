02023c7c5d1c6ca6f42ab306204c0f39221940e9
hatlonely <hatlonely@gmail.com>
2022-02-13 16:49:29 +0800

    update

--- qas/reporter/text_reporter.py
+++ qas/reporter/text_reporter.py
@@ -7,7 +7,8 @@ import re
 import durationpy
 from colorama import Fore
 from .reporter import Reporter
-from ..result import TestResult, CaseResult, StepResult, ExpectResult
+from ..result import TestResult, CaseResult
+from .format_step_res import format_step_res
 
 
 class TextReporter(Reporter):
@@ -92,49 +93,13 @@ class TextReporter(Reporter):
 
         for sub_step in step.sub_steps:
             lines.extend(("req: " + json.dumps(sub_step.req, default=lambda x: str(x), indent=2)).split("\n"))
-
             if sub_step.is_err:
                 lines.extend(("res: " + json.dumps(sub_step.res, indent=2)).split("\n"))
                 lines.extend(["  " + i for i in sub_step.err.split("\n")])
                 return lines
-
-            # 修改 res 返回值，将预期值标记后拼接在 value 后面
-            for expect_result in sub_step.assertions:
-                if expect_result.is_pass:
-                    TextReporter.append_val_to_key(sub_step.res, expect_result.node, "<GREEN>{}<END>".format(expect_result.expect))
-                else:
-                    TextReporter.append_val_to_key(sub_step.res, expect_result.node, "<RED>{}<END>".format(expect_result.expect))
-
-            res_lines = ("res: " + json.dumps(sub_step.res, indent=2)).split("\n")
-            format_lines = []
-            # 解析 res 中 value 的值，重新拼接成带颜色的结果值
-            for line in res_lines:
-                mr = re.match(r'(\s+".*?": )"(.*)<GREEN>(.*)<END>"(.*)', line)
-                if mr:
-                    format_lines.append("{}{}{} # {}{}{}".format(
-                        mr.groups()[0], json.loads('"{}"'.format(mr.groups()[1])), mr.groups()[3], Fore.GREEN, mr.groups()[2], Fore.RESET,
-                    ))
-                    continue
-                mr = re.match(r'(\s+".*?": )"(.*)<RED>(.*)<END>"(.*)', line)
-                if mr:
-                    format_lines.append("{}{}{} # {}{}{}".format(
-                        mr.groups()[0], json.loads('"{}"'.format(mr.groups()[1])), mr.groups()[3], Fore.RED, mr.groups()[2], Fore.RESET,
-                    ))
-                    continue
-                mr = re.match(r'(\s+)"(.*)<GREEN>(.*)<END>"(.*)', line)
-                if mr:
-                    format_lines.append("{}{}{} # {}{}{}".format(
-                        mr.groups()[0], json.loads('"{}"'.format(mr.groups()[1])), mr.groups()[3], Fore.GREEN, mr.groups()[2], Fore.RESET,
-                    ))
-                    continue
-                mr = re.match(r'(\s+)"(.*)<RED>(.*)<END>"(.*)', line)
-                if mr:
-                    format_lines.append("{}{}{} # {}{}{}".format(
-                        mr.groups()[0], json.loads('"{}"'.format(mr.groups()[1])), mr.groups()[3], Fore.RED, mr.groups()[2], Fore.RESET,
-                    ))
-                    continue
-                format_lines.append(line)
-            lines.extend(format_lines)
+            lines.extend(format_step_res(
+                sub_step, pass_open=Fore.GREEN, pass_close=Fore.RESET, fail_open=Fore.RED, fail_close=Fore.RESET,
+            ).split("\n"))
         return lines
 
     @staticmethod
