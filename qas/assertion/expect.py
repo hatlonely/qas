363e4990fd94b91552b023e831cfc7d16ceac830
hatlonely <hatlonely@gmail.com>
2022-02-13 16:23:24 +0800

    update

--- qas/assertion/expect.py
+++ qas/assertion/expect.py
@@ -7,16 +7,16 @@ from ..util.include import *
 
 def expect(vals, rules, case=None, step=None, var=None, x=None):
     results = []
-    _expect("", results, vals, rules, case=case, step=step, var=var, x=x)
+    _expect_recursive("", results, vals, rules, case=case, step=step, var=var, x=x)
     return results
 
 
-def _expect(root: str, results: list[ExpectResult], vals, rules, case=None, step=None, var=None, x=None):
+def _expect_recursive(root: str, results: list[ExpectResult], vals, rules, case=None, step=None, var=None, x=None):
     if isinstance(rules, dict):
         for key, rule in rules.items():
             root_dot_key = "{}.{}".format(root, str(key).lstrip("#")).lstrip(".")
             if isinstance(rule, dict) or isinstance(rule, list):
-                _expect(root_dot_key, results, vals[key], rule, case=case, step=step, var=var, x=x)
+                _expect_recursive(root_dot_key, results, vals[key], rule, case=case, step=step, var=var, x=x)
             else:
                 if key.startswith("#"):
                     results.append(run_expect(root_dot_key, rule, "match", val=vals[key[1:]], case=case, step=step, var=var, x=x))
@@ -26,7 +26,7 @@ def _expect(root: str, results: list[ExpectResult], vals, rules, case=None, step
         for idx, rule in enumerate(rules):
             root_dot_key = "{}.{}".format(root, idx).lstrip(".")
             if isinstance(rule, dict) or isinstance(rule, list):
-                _expect(root_dot_key, results, vals[idx], rule, case=case, step=step, var=var, x=x)
+                _expect_recursive(root_dot_key, results, vals[idx], rule, case=case, step=step, var=var, x=x)
             else:
                 results.append(run_expect(root_dot_key, rule, "equal", val=vals[idx], case=case, step=step, var=var, x=x))
 
