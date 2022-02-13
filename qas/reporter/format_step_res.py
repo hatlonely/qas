404983695ff9093c91fb61ba67694183ea2427c9
hatlonely <hatlonely@gmail.com>
2022-02-13 17:01:18 +0800

    update

--- qas/reporter/format_step_res.py
+++ qas/reporter/format_step_res.py
@@ -7,7 +7,7 @@ import json
 from ..result import SubStepResult
 
 
-def format_step_res(sub_step: SubStepResult, pass_open="", pass_close="", fail_open="", fail_close="") -> str:
+def format_step_res(sub_step: SubStepResult, separator="#", pass_open="", pass_close="", fail_open="", fail_close="") -> str:
     # 修改 res 返回值，将预期值标记后拼接在 value 后面
     for expect_result in sub_step.assertions:
         if expect_result.is_pass:
@@ -21,26 +21,26 @@ def format_step_res(sub_step: SubStepResult, pass_open="", pass_close="", fail_o
     for line in res_lines:
         mr = re.match(r'(\s+".*?": )"(.*)<GREEN>(.*)<END>"(.*)', line)
         if mr:
-            lines.append("{}{}{} # {}{}{}".format(
-                mr.groups()[0], json.loads('"{}"'.format(mr.groups()[1])), mr.groups()[3], pass_open, mr.groups()[2], pass_close
+            lines.append("{}{}{} {} {}{}{}".format(
+                mr.groups()[0], json.loads('"{}"'.format(mr.groups()[1])), mr.groups()[3], separator, pass_open, mr.groups()[2], pass_close
             ))
             continue
         mr = re.match(r'(\s+".*?": )"(.*)<RED>(.*)<END>"(.*)', line)
         if mr:
-            lines.append("{}{}{} # {}{}{}".format(
-                mr.groups()[0], json.loads('"{}"'.format(mr.groups()[1])), mr.groups()[3], fail_open, mr.groups()[2], fail_close
+            lines.append("{}{}{} {} {}{}{}".format(
+                mr.groups()[0], json.loads('"{}"'.format(mr.groups()[1])), mr.groups()[3], separator, fail_open, mr.groups()[2], fail_close
             ))
             continue
         mr = re.match(r'(\s+)"(.*)<GREEN>(.*)<END>"(.*)', line)
         if mr:
-            lines.append("{}{}{} # {}{}{}".format(
-                mr.groups()[0], json.loads('"{}"'.format(mr.groups()[1])), mr.groups()[3], pass_open, mr.groups()[2], pass_close
+            lines.append("{}{}{} {} {}{}{}".format(
+                mr.groups()[0], json.loads('"{}"'.format(mr.groups()[1])), mr.groups()[3], separator, pass_open, mr.groups()[2], pass_close
             ))
             continue
         mr = re.match(r'(\s+)"(.*)<RED>(.*)<END>"(.*)', line)
         if mr:
-            lines.append("{}{}{} # {}{}{}".format(
-                mr.groups()[0], json.loads('"{}"'.format(mr.groups()[1])), mr.groups()[3], fail_open, mr.groups()[2], fail_close
+            lines.append("{}{}{} {} {}{}{}".format(
+                mr.groups()[0], json.loads('"{}"'.format(mr.groups()[1])), mr.groups()[3], separator, fail_open, mr.groups()[2], fail_close
             ))
             continue
         lines.append(line)
