ae0ecc3bcfc14c926b1bc6fa270de58f206913ed
hatlonely <hatlonely@gmail.com>
2022-02-19 17:50:58 +0800

    update

--- qas/hook/debug_hook.py
+++ qas/hook/debug_hook.py
@@ -18,42 +18,49 @@ class DebugHook(Hook):
         self.padding = ""
 
     def on_test_start(self, test):
-        print("{} 测试开始 {}".format(self.padding, test))
+        print("{}{i18n.title.test} {name}".format(self.padding, name=test, i18n=self.i18n))
         self.padding += self.padding_to_add
 
     def on_test_end(self, res: TestResult):
         self.padding = self.padding[:-len(self.padding_to_add)]
-        print("{} 测试结束 {} {}".format(self.padding, res.directory, json.dumps(res, default=lambda x: x.to_json())))
+        print("{}{i18n.title.test} {res.name}".format(self.padding, res=res, i18n=self.i18n))
 
     def on_case_start(self, case_info):
-        print("{} case 开始 {} {}".format(self.padding, case_info["name"], json.dumps(case_info)))
+        print("{}{i18n.testHeader.case} {name}".format(self.padding, name=case_info["name"], i18n=self.i18n))
         self.padding += self.padding_to_add
+        DebugHook.debug_object(self.padding, "CaseInfo", case_info)
 
     def on_case_end(self, res: CaseResult):
         self.padding = self.padding[:-len(self.padding_to_add)]
-        print("{} case 结束 {} {}".format(self.padding, res.name, json.dumps(res, default=lambda x: x.to_json())))
+        print("{}{i18n.testHeader.case} {res.name}".format(self.padding, res=res, i18n=self.i18n))
 
     def on_set_up_start(self, case_info):
-        print("{} set_up 开始 {} {}".format(self.padding, case_info["name"], json.dumps(case_info)))
+        print("{}{i18n.testHeader.setUp} {name}".format(self.padding, name=case_info["name"], i18n=self.i18n))
         self.padding += self.padding_to_add
+        DebugHook.debug_object(self.padding, "SetUpInfo", case_info)
 
     def on_set_up_end(self, res: CaseResult):
         self.padding = self.padding[:-len(self.padding_to_add)]
-        print("{} set_up 结束 {} {}".format(self.padding, res.name, json.dumps(res, default=lambda x: x.to_json())))
+        print("{}{i18n.testHeader.setUp} {res.name}".format(self.padding, res=res, i18n=self.i18n))
 
     def on_tear_down_start(self, case_info):
-        print("{} tear_down 开始 {} {}".format(self.padding, case_info["name"], json.dumps(case_info)))
+        print("{}{i18n.testHeader.tearDown} {name}".format(self.padding, name=case_info["name"], i18n=self.i18n))
         self.padding += self.padding_to_add
+        DebugHook.debug_object(self.padding, "TearDownInfo", case_info)
 
     def on_tear_down_end(self, res: CaseResult):
         self.padding = self.padding[:-len(self.padding_to_add)]
-        print("{} tear_down 结束 {} {}".format(self.padding, res.name, json.dumps(res, default=lambda x: x.to_json())))
+        print("{}{i18n.testHeader.tearDown} {res.name}".format(self.padding, res=res, i18n=self.i18n))
 
     def on_step_start(self, step_info):
-        print("{} step 开始 {} {}".format(self.padding, step_info["name"], json.dumps(step_info)))
+        print("{}{i18n.caseHeader.step} {name}".format(self.padding, name=step_info["name"], i18n=self.i18n))
         self.padding += self.padding_to_add
+        DebugHook.debug_object(self.padding, "Step", step_info)
 
     def on_step_end(self, res: StepResult):
         self.padding = self.padding[:-len(self.padding_to_add)]
-        print("{} step 结束 {} {}".format(self.padding, res.name, json.dumps(res, default=lambda x: x.to_json())))
+        print("{}{i18n.caseHeader.step} {res.name}".format(self.padding, res=res, i18n=self.i18n))
 
+    @staticmethod
+    def debug_object(padding, title, result):
+        print("\n".join([padding + line for line in ("{}: {}".format(title, json.dumps(result, indent=2))).split("\n")]))
