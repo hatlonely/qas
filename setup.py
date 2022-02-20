7797defd125102a493d219e8ff6a1b9b6fc0f47f
hatlonely <hatlonely@gmail.com>
2022-02-20 22:07:17 +0800

    update

--- setup.py
+++ setup.py
@@ -12,10 +12,6 @@ with open("LICENSE") as fp:
     license_ = fp.read()
 
 
-with open("requirements.txt") as fp:
-    requires = [i.strip() for i in fp.readlines()]
-
-
 setup(
     name="qas",
     version="1.0.1",
@@ -28,5 +24,29 @@ setup(
     packages=find_packages(exclude=("tests", "docs", "tmp", "ops", "build", "dist")),
     scripts=["bin/qas"],
     keywords=["qas"],
-    install_requires=requires,
+    install_requires=[
+        "requests~=2.27.1",
+        "PyYAML~=6.0",
+        "colorama~=0.4.4",
+        "tablestore~=5.1.0",
+        "python-dateutil~=2.8.1",
+        "aliyun_python_sdk_core==2.13.30",
+        "aliyunsdkcore~=1.0.3",
+        "PyMySQL~=1.0.2",
+        "redis~=4.1.0",
+        "setuptools~=57.4.0",
+        "durationpy~=0.5",
+        "aliyun-mns-sdk~=1.1.6",
+        "oss2~=2.13.0",
+        "pymongo~=4.0.1",
+        "pycrypto~=2.6.1",
+        "Jinja2~=3.0.3",
+        "Markdown~=3.3.6",
+        "grpcio~=1.43.0",
+        "grpcio-tools~=1.43.0",
+        "protobuf~=3.19.3",
+        "thrift~=0.15.0",
+        "elasticsearch~=7.16.0",
+        "aliyun-log-python-sdk~=0.7.5"
+    ],
 )
