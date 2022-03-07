3a003215cd8c4e30b209c341739e5d6494fc7f66
hatlonely <hatlonely@gmail.com>
2022-03-07 17:42:17 +0800

    update

--- qas/driver/grpc_driver.py
+++ qas/driver/grpc_driver.py
@@ -17,27 +17,35 @@ class GRPCDriver(Driver):
         args = merge(args, {
             "proto": REQUIRED,
             "endpoint": REQUIRED,
-            "service": REQUIRED
+            "service": REQUIRED,
+            "include": [],
         })
         p = pathlib.Path(args["proto"])
         self.proto_path = p
-        self.generate_proto()
+        self.generate_proto(self.proto_path, args["include"])
+        self.include = args["include"]
         sys.path.append(str(p.parent.absolute()))
-        self.pb2 = importlib.import_module(p.stem + "_pb2")
-        self.pb2_grpc = importlib.import_module(p.stem + "_pb2_grpc")
+        self.pb2 = importlib.import_module(p.stem.replace("-", "_") + "_pb2")
+        self.pb2_grpc = importlib.import_module(p.stem.replace("-", "_") + "_pb2_grpc")
         channel = grpc.insecure_channel(args["endpoint"])
-        self.stub = getattr(self.pb2_grpc, args["service"])(channel)
+        self.stub = getattr(self.pb2_grpc, args["service"] + "Stub")(channel)
 
-    def generate_proto(self):
+    @staticmethod
+    def generate_proto(proto_path, include):
         command = "python3 -m grpc_tools.protoc " \
-                  "-I{directory} --python_out={directory} " \
+                  "-I{directory} {include} --python_out={directory} " \
                   "--grpc_python_out={directory} {source}".format(
-                    directory=str(self.proto_path.parent), source=str(self.proto_path))
-        subprocess.run(
+                    directory=str(proto_path.parent),
+                    source=str(proto_path),
+                    include=" ".join(["-I" + i for i in include]),
+                  )
+        process = subprocess.run(
             command.split(),
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
         )
+        if process.returncode != 0:
+            raise Exception("{}\n{}".format(command, process.stderr))
 
     def name(self, req):
         return req["method"]
